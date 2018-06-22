9b284bf71c286ef6268d9c31a2385916629f1eb8
liuweipeng <liuweipeng@hongte.info>
2018-06-22 17:28:41 +0800

    初始化java工具类

---
+++ src/main/java/com/lwp/util/DateUtil.java
@@ -0,0 +1,9 @@
+package com.lwp.util;
+
+/**
+ * @Auther: liuweipeng
+ * @Date: 2018/6/22 17:20
+ * @Description: 时间工具类
+ */
+public class DateUtil {
+}
---
+++ src/main/java/com/lwp/util/MapUtil.java
@@ -0,0 +1,9 @@
+package com.lwp.util;
+
+/**
+ * @Auther: liuweipeng
+ * @Date: 2018/6/22 17:20
+ * @Description: Map与实例对象之间的转换工具类
+ */
+public class MapUtil {
+}
