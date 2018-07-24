a486b7bd1cb0cdae312fce0ff45b97d36b45f43c
liuweipeng <liuweipeng@hongte.info>
2018-07-24 16:37:07 +0800

    新增MD5工具类

---
+++ src/main/java/com/lwp/util/MD5Util.java
@@ -0,0 +1,9 @@
+package com.lwp.util;
+
+/**
+ * @Auther: liuweipeng
+ * @Date: 2018/7/24 16:34
+ * @Description:
+ */
+public class MD5Util {
+}
--- src/main/java/com/lwp/util/MapUtil.java
+++ src/main/java/com/lwp/util/MapUtil.java
@@ -1,9 +1,17 @@
 package com.lwp.util;
 
+import java.util.Map;
+
 /**
  * @Auther: liuweipeng
  * @Date: 2018/6/22 17:20
  * @Description: Map与实例对象之间的转换工具类
  */
 public class MapUtil {
+    public static Object mapToObject(){
+        return null;
+    }
+    public static Map<String,Object> ObjectToMap(){
+        return null;
+    }
 }
