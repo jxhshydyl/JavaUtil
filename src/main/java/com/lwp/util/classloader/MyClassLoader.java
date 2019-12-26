906e21c5a6472efadbd0076b9f91135b11c3c3ed
liuweipeng <dad9479@dingtalk.com>
2019-12-26 11:10:00 +0800

    类加载器

--- src/main/java/com/lwp/util/classloader/MyClassLoader.java
+++ src/main/java/com/lwp/util/classloader/MyClassLoader.java
@@ -9,17 +9,16 @@ import java.io.InputStream;
 
 public class MyClassLoader extends ClassLoader{
 
-    private String classpath;
-    
-    public MyClassLoader(String classpath) {
-        
-        this.classpath = classpath;
+    private String classPath;
+
+    public MyClassLoader(String classPath) {
+        this.classPath=classPath;
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         try {
-            byte [] classDate=getDate(name);
+            byte [] classDate=getDate(classPath);
             
             if(classDate==null){}
             
@@ -35,14 +34,31 @@ public class MyClassLoader extends ClassLoader{
         
         return super.findClass(name);
     }
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        // app类加载器  加载classpath下的class  ext加载器 加载 jre/lib/jext   bootstarp加载器加载 jre/lib
+        //获取app类加载器的父加载器  防止app加载器 加载classpath下的class
+        ClassLoader systemClassLoader = getSystemClassLoader().getParent();
+        Class<?> aClass = null;
+        try {
+            // 如果 name 不在jre/lib/jext 或 jre/lib下 会 抛ClassNotFound异常
+            aClass = systemClassLoader.loadClass(name);
+        }catch (Exception e){
+
+        }
+        if(aClass == null){
+            return findClass(name);
+        }
+        return aClass;
+    }
+
     //返回类的字节码
     private byte[] getDate(String className) throws IOException{
         InputStream in = null;
         ByteArrayOutputStream out = null;
-        String path=classpath + File.separatorChar +
-                    className.replace('.',File.separatorChar)+".class";
         try {
-            in=new FileInputStream(path);
+            in=new FileInputStream(className);
             out=new ByteArrayOutputStream();
             byte[] buffer=new byte[2048];
             int len=0;
--- src/main/java/com/lwp/util/classloader/TestMyClassLoader.java
+++ src/main/java/com/lwp/util/classloader/TestMyClassLoader.java
@@ -5,17 +5,18 @@ import java.lang.reflect.Method;
 
 public class TestMyClassLoader {
 
-    public static void main(String []args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException{
+    public static void main(String []args) throws Exception{
         //自定义类加载器的加载路径
-        MyClassLoader myClassLoader=new MyClassLoader("com.lwp.util");
+        MyClassLoader myClassLoader=new MyClassLoader("D:\\IDEAWoekspace\\atop-java\\ex-public\\target\\classes\\com\\ex\\expublic\\model\\dto\\SysMaintainTimeDto.class");
         //包名+类名
-        Class c=myClassLoader.loadClass("com.test.Test");
-        
-        if(c!=null){
-            Object obj=c.newInstance();
-            Method method=c.getMethod("say", null);
-            method.invoke(obj, null);
-            System.out.println(c.getClassLoader().toString());
-        }
+        Class c=myClassLoader.loadClass("com.ex.expublic.model.dto.SysMaintainTimeDto");
+        System.out.println(c.getClassLoader().toString());
+
+        //自定义类加载器的加载路径
+        MyClassLoader myClassLoader1=new MyClassLoader("D:\\IDEAWoekspace\\JavaUtil\\target/classes/com/lwp/util/classloader/MyClassLoader.class");
+        //包名+类名
+        Class c1=myClassLoader1.loadClass("com.lwp.util.classloader.MyClassLoader");
+
+        System.out.println(c1.getClassLoader().toString());
     }
 }
